d3be21e1cd7433620c32934c7b00de9b09fedd98
Atul Venkatesh <74465636+Atuav10@users.noreply.github.com>
2025-04-13 14:33:36 -0400

    Update code.R

--- code.R
+++ code.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 library(igraph)
 library(nflfastR)
-df <- read.csv("qss41_final_data.csv", stringsAsFactors = FALSE)
+df <- read.csv("code.csv", stringsAsFactors = FALSE)
 table(df$position)
 #Create a function that turns our data into connections between coaches based on team and year
 create_coach_connections <- function(data) {
